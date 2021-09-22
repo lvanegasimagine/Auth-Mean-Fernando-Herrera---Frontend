c048ed96eaa3b09992203e002cb1971b0df905e9
Luis Vanegas <lvanegas1429@gmail.com>
2021-09-22 10:13:47 -0600

    Implementando el canactivate

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { catchError, map, tap } from "rxjs/operators";
 import { AuthResponse, Usuario } from '../interfaces/auth.interfaces';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -41,10 +41,18 @@ export class AuthService {
     );
   }
 
-  validarToken(){
+  validarToken(): Observable<boolean>{
     const url = `${this.baseUrl}/auth/renew`;
 
     const headers = new HttpHeaders().set('x-token', localStorage.getItem('token') || '');
-    return this.http.get(url, { headers });
+    return this.http.get<AuthResponse>(url, { headers })
+    .pipe(map(resp => {
+      localStorage.setItem('token', resp.token!);
+          this._usuario = {
+            name: resp.name!,
+            uid: resp.uid!
+          }
+      return resp.ok;
+    }),catchError(err => of(false)));
   }
 }
